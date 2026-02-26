13d14f974f65ac5bc320ee09ca8236aa0063e794
Caio da Silveira Leal Granja <lealcaio2004@gmail.com>
2026-02-26 15:13:10 -0300

    Comentários js

--- JavaScript/executar-codigo/instrucoes.js
+++ JavaScript/executar-codigo/instrucoes.js
@@ -1,3 +1,11 @@
 console.log("quebre 3 ovos")
 console.log("Misture com a farinha")
 console.log("leve ao forno")
+
+//Comentário: O código acima é um exemplo de instruções para fazer um bolo. Ele lista os passos necessários para preparar a massa do bolo.
+
+/* 
+
+Para comentar um bloco de código, deve-se utilizar essa sintaxe.
+
+*/
