41be179bca37d07ee2d49bf5cc14df8451347582
Caio da Silveira Leal Granja <lealcaio2004@gmail.com>
2026-02-26 15:29:45 -0300

    Início de Variáveis

--- JavaScript/executar-codigo/instrucoes.js
+++ JavaScript/executar-codigo/instrucoes.js
@@ -9,3 +9,11 @@ console.log("leve ao forno")
 Para comentar um bloco de código, deve-se utilizar essa sintaxe.
 
 */
+
+let altura = 1.70
+let peso = 70
+//let imc = peso / (altura * altura)
+const pi = 3.14
+
+console.log("O IMC é: " + (peso / (altura * altura)))
+console.log("O valor de pi é: " + pi)
