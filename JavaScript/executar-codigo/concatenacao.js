ac0e7316aeb6878b75a08216cb98db45172fec2b
Caio da Silveira Leal Granja <lealcaio2004@gmail.com>
2026-02-27 19:51:30 -0300

    Aula template String

---
+++ JavaScript/executar-codigo/concatenacao.js
@@ -0,0 +1,8 @@
+const nome = "Caio"
+const numero = 2
+
+//console.log(nome + " tem " + numero + " itens no carrinho")
+
+const texto = `Olá, ${nome}! Você tem ${numero} itens no carrinho.`
+
+console.log(texto)
