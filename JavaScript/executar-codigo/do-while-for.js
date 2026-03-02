eff23969d0775beb915703b683ba54750ae16b92
Caio da Silveira Leal Granja <lealcaio2004@gmail.com>
2026-03-02 19:05:21 -0300

    Aula do-while-for

---
+++ JavaScript/executar-codigo/do-while-for.js
@@ -0,0 +1,18 @@
+//while
+// let numero = 1
+// while(numero <= 5) {
+//     console.log("Execução " + numero);
+//     numero++;
+// }
+
+//do while
+// let numero = 0
+//  do{
+//     console.log("Execução " + numero);
+//     numero--;
+// }while(numero <= 1)
+
+//for
+for(let numero = 5; numero >= 1; numero--){
+    console.log("Execução " + numero);
+}
