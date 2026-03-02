9b992a876ecd0d3f5f4223a55553e5fb8efeedea
Caio da Silveira Leal Granja <lealcaio2004@gmail.com>
2026-03-02 20:16:01 -0300

    Aula Funções e Arrows

---
+++ JavaScript/executar-codigo/funcoes&arrow.js
@@ -0,0 +1,17 @@
+// function somar (a,b){
+//     console.log(a + b)
+// }
+
+// somar(1,2)
+
+// const somar = function (a,b){
+//     console.log(a + b)
+// }
+
+// somar(1,2)
+
+const somar = (a,b) => {
+    console.log(a + b)
+}
+
+somar(1,2)
