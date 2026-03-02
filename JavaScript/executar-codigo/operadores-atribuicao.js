4364847bf2ad3a7f18e4aeee0be44878f9c0ae66
Caio da Silveira Leal Granja <lealcaio2004@gmail.com>
2026-03-02 19:41:48 -0300

    Aula Operadores de Atribuição

---
+++ JavaScript/executar-codigo/operadores-atribuicao.js
@@ -0,0 +1,11 @@
+let salario = 2000
+
+salario += 500 // salario = salario + 500
+
+salario -= 300 // salario = salario - 300
+
+salario *= 2 // salario = salario * 2
+
+salario /= 2 // salario = salario / 2
+
+console.log(salario)
