78ff17bb84b46e7120f47ee660cb9725f2c5ef53
Caio da Silveira Leal Granja <lealcaio2004@gmail.com>
2026-03-02 19:33:09 -0300

    Projeto Pesquisar e Carregar Nomes

---
+++ JavaScript/projeto-lista-nomes/script.js
@@ -0,0 +1,44 @@
+let nomes = [
+    "joão",
+    "maria",
+    "pedro",
+    "ana",
+    "lucas",
+    "caio",
+    "beatriz",
+    "fernanda",
+    "rafael",
+    "camila",
+    "sofia",
+    "gustavo",
+    "larissa",
+    "caio",
+]
+
+function pesquisarNome() {
+
+    let nomePesquisa = document.getElementById('campoNome').value
+    let itensLista = ''
+
+    for(indice in nomes) {
+        let nome = nomes[indice]
+        if(nomePesquisa == nome){
+            itensLista += `<li class="list-group-item">${nome}</li>`
+        }
+    }
+    
+    document.getElementById('lista').innerHTML = itensLista
+}
+
+function carregarNomes() {
+
+
+    let itensLista = ''
+    for(indice in nomes){
+        let nome = nomes[indice]
+
+        itensLista += `<li class="list-group-item">${nome}</li>`
+    }
+
+    document.getElementById('lista').innerHTML = itensLista
+}
