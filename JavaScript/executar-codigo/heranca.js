0d181e7e6eb49e0b4ea70e1209374e7ba8c633d1
Caio da Silveira Leal Granja <lealcaio2004@gmail.com>
2026-03-04 18:28:04 -0300

    1° Aula Herança

---
+++ JavaScript/executar-codigo/heranca.js
@@ -0,0 +1,33 @@
+//Herança
+class Animal{ //Superclasse ou classe pai
+    constructor(){
+        this.cor = ""
+        this.tamanho = 0
+        this.peso = 0
+    }
+
+    correr(){
+        console.log("correndo.")
+    }
+
+    dormir(){
+        console.log("dormindo.")
+    }
+}
+
+class Cao extends Animal{ //Subclasse ou classe filha
+
+    latir(){
+        console.log("O cachorro está latindo.")
+    }
+}
+
+class Passaro extends Animal{ //Subclasse ou classe filha
+    
+    voar(){
+        console.log("O pássaro está voando.")
+    }
+}
+
+const cao = new Cao();
+const passaro = new Passaro();
