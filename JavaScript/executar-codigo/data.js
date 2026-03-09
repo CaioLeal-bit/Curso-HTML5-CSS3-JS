b19225c09b56739398044440d50ab61893cc01e5
Caio da Silveira Leal Granja <lealcaio2004@gmail.com>
2026-03-09 20:38:37 -0300

    Aula sobre Datas

---
+++ JavaScript/executar-codigo/data.js
@@ -0,0 +1,11 @@
+const data = new Date();
+data.toString()
+console.log(data.toString())
+
+data.getDate() // dia do mês
+data.getDay() // dia da semana
+data.getFullYear() // ano
+
+data.getHours() // hora
+data.getMinutes() // minutos
+data.getSeconds() // segundos
