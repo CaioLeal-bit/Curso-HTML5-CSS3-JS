b58f1818b08b8bfe0c4247133dd1da9ce33330ff
Caio da Silveira Leal Granja <lealcaio2004@gmail.com>
2026-03-09 20:46:10 -0300

    2a Aula de arrays

---
+++ JavaScript/executar-codigo/array+.js
@@ -0,0 +1,7 @@
+const nomes = ['João', 'Maria', 'Pedro', 'Ana'];
+const frutas = ['Maçã', 'Banana', 'Laranja', 'Uva'];
+console.log(frutas[2]); // Laranja
+console.log(nomes[0]); // João  
+frutas.pusgh('Abacaxi');
+console.log(frutas); // ['Maçã', 'Banana', 'Laranja', 'Uva', 'Abacaxi']
+
