8558f44752719e1e5dbc287c8fc1f13ee1e921a6
Caio da Silveira Leal Granja <lealcaio2004@gmail.com>
2026-03-09 20:27:19 -0300

    Funções Matemáticas

---
+++ JavaScript/executar-codigo/funcoes-matematica.js
@@ -0,0 +1,14 @@
+let numero = 280.50
+
+// Math.PI
+Math.ceil(numero); // 281
+Math.floor(numero); // 280
+Math.round(numero); // 281
+Math.random() * 100; // número aleatório entre 0 e 100
+Math.max(10, 20, 5); // 20
+Math.min(10, 20, 5); // 5
+Math.pow(2, 3); // 8
+Math.sqrt(16); // 4
+Math.abs(-5); // 5
+Math.sign(-10); // -1
+Math.trunc(4.9); // 4
