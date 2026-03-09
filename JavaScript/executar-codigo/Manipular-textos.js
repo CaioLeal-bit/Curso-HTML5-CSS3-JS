87bbe59de8f8ee13018dd26533f76433a7a0933d
Caio da Silveira Leal Granja <lealcaio2004@gmail.com>
2026-03-09 19:08:18 -0300

    Aula sobre Manipulação de Textos

---
+++ JavaScript/executar-codigo/Manipular-textos.js
@@ -0,0 +1,15 @@
+/*const nome = {
+    texto: "Caio",
+    length: 4,
+    charAt(){}
+}*/
+
+let nome = "Caio Leal   ";
+nome.charAt(0); // C
+nome.charAt(1); // a
+let n = nome.replace("Caio", "João"); // João Leal
+let resultado = nome.split(" "); // ["Caio", "Leal"]
+nome.toUpperCase(); // CAIO LEAL
+nome.toLowerCase(); // caio leal
+nome.trim(); // "Caio Leal"
+
